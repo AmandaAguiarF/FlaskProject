2530b10986f6329da0cf95658d21f966d411a8a4
AmandaAguiarF <amanda_aguiarfontes@hotmail.com>
2024-07-25 00:02:13 -0300

    Criação do model, controller e view

---
+++ src/models/employeeModel.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# Para instanciar o banco de dados
+db = SQLAlchemy()
+
+# Criação da classe
+class EmployeeModel():
+
+    __tablename__ = 'employee'
+
+    id = db.Column(db.Integer, primary_key= True)
+    cpf = db.Column(db.String(11), unique = True)
+    name = db.Column(db.String(100))
+    position = db.Column(db.String(20))
+
+
+# Criação do construtor
+    def __init__(self, cpf, name, position):
+        self.cpf = cpf
+        self.name = name
+        self.position = position
